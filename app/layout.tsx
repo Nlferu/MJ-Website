779a3e2b312bb04089619ff0c4561e273c0eabd3
Niferu <wargacki.patryq@gmail.com>
2024-01-10 16:59:29 +0100

    building header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "@/styles/globals.css"
+import Header from "@/components/header"
 
+// Icon font: Balgin
+// Website Font: Cormorant Garamond
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -12,7 +15,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={`${inter.className} bg-gray-200`}>
+                {children}
+                <Header />
+            </body>
         </html>
     )
 }
---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>x y z</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}

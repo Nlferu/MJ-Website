756300673f7af26433e2829dce0af4d488da8841
Niferu <wargacki.patryq@gmail.com>
2024-01-11 18:00:47 +0100

    navbar styles and animation

--- components/header.tsx
+++ components/header.tsx
@@ -25,12 +25,13 @@ export default function Header() {
 
     return (
         <header className="relative z-[999]">
-            <nav
-                className={clsx("fixed flex top-0 h-[2rem] w-full items-center justify-center transition-all duration-1000", {
-                    "fixed flex top-0 !h-[4.5rem] w-full items-center justify-center !transition-all !duration-1000": scrollPosition === 0,
-                })}
-            >
-                <ul className="flex gap-[7rem] text-2xl">
+            <nav className="fixed flex top-0 h-[4.5rem] w-full items-center justify-center transition-all duration-1000">
+                <ul
+                    className={clsx("flex items-center justify-center gap-[7rem] w-full text-2xl transition-all duration-1000", {
+                        "!w-full flex items-center justify-center border-b-[1px] border-t-[1px] border-black bg-black/20 px-[1.5rem] py-[0.7rem] gap-[2rem] backdrop-blur-md !transition-all !duration-1000":
+                            scrollPosition !== 0,
+                    })}
+                >
                     {links.map((link) => (
                         <li className="" key={link.hash}>
                             <Link

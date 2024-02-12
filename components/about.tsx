18b878cd89c45b93ced04a50b89396dd475179cd
Niferu <wargacki.patryq@gmail.com>
2024-02-12 14:10:04 +0100

    apos instead of '

--- components/about.tsx
+++ components/about.tsx
@@ -18,15 +18,17 @@ export default function About() {
                 transition={{ delay: 0.5, duration: 1 }}
                 viewport={{ once: true }}
             >
-                Hello! I'm Marta. Since high school, I have known that I want to be an accountant. Determined to turn my passion into a profession, I pursued a
-                comprehensive education, earning both a bachelor's and master's degree from the prestigious University of Economics in Krakow.
+                Hello! I&apos;m Marta. Since high school, I have known that I want to be an accountant. Determined to turn my passion into a profession, I
+                pursued a comprehensive education, earning both a bachelor&apos;s and master&apos;s degree from the prestigious University of Economics in
+                Krakow.
                 <span className="my-4">
                     My professional journey has taken me through diverse landscapes—from a dynamic small accounting office to a bustling corporation, and
                     finally to a unique hybrid that seamlessly blends both sectors. Along the way, I took charge of managing a branch of an accounting office,
                     gaining invaluable hands-on experience in human resources management.
                 </span>
-                With two years of expertise under my belt in the latter area, I'm driven to continuously enhance my skills in this ever-evolving field and
-                ambitiously expand my competencies. I'm fueled by a genuine passion for the intricacies of accounting and the dynamic realm of human resources.
+                With two years of expertise under my belt in the latter area, I&apos;m driven to continuously enhance my skills in this ever-evolving field and
+                ambitiously expand my competencies. I&apos;m fueled by a genuine passion for the intricacies of accounting and the dynamic realm of human
+                resources.
             </motion.div>
         </section>
     )
